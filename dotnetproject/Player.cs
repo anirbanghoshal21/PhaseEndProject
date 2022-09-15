834e523992197c674f02e891fa6cfcbfaac62768
username <username@cognizant.com>
2022-09-15 17:47:35 +0530

    Add project files.

---
+++ dotnetproject/Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace PhaseEndProject
+{
+    class Player
+    {
+        public int PlayerId;
+        public string PlayerName;
+        public int PlayerAge;
+    }
+
+    interface ITeam
+    {
+       void Add(Player player);
+       void Remove(int playerid);
+       Player GetPlayerById(int playerId);
+       Player GetPlayerByName(string playerName);
+       List<Player> GetAllPlayers();
+    }
+    class OneDayTeam : ITeam
+    {
+        public static List<Player> oneDayTeam = new List<Player>();
+        public OneDayTeam()
+        {
+        }
+        public void Add(Player player)
+        {
+            oneDayTeam.Add(player);
+            //Console.WriteLine("{0},  {1},   {2} ", oneDayTeam[0].PlayerId, oneDayTeam[0].PlayerName, oneDayTeam[0].PlayerAge);
+        }
+        public void Remove(int playerId)
+        {
+            oneDayTeam.Remove(GetPlayerById(playerId));
+        }
+        public Player GetPlayerById(int playerId)
+        {
+            Player plr=null;
+            int i = 0;
+            foreach (Player p in oneDayTeam)
+            {
+                if (p.PlayerId == playerId)
+                {
+                    plr = oneDayTeam[i];
+                    break;
+                }
+                i++;
+            }
+            return plr;
+        }
+        public Player GetPlayerByName(string playerName)
+        {
+            Player plr = null;
+            foreach (Player p in oneDayTeam)
+            {
+                if (p.PlayerName.Equals(playerName))
+                {
+                    plr = p;
+                    break;
+                }
+            }
+            return plr;
+        }
+        public List<Player> GetAllPlayers()
+        {
+            return oneDayTeam;
+        }
+
+    }
+}
---
+++ dotnetproject/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhaseEndProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice2 = "yes";
+            int choice1= 1;
+            
+            OneDayTeam odt = new OneDayTeam();
+            int pid;
+            while (choice2.Equals("yes"))
+            {
+                Console.Write("\nEnter 1:To Add Player 2:To Remove Player by Id 3.Get Player By Id 4.Get Player by Name 5.Get All Players:");
+                choice1 = int.Parse(Console.ReadLine());
+                switch (choice1)
+                {
+                    case 1:
+                        Player plr = new Player();
+                        Console.Write("\nEnter Player Id: ");
+                        plr.PlayerId=int.Parse(Console.ReadLine());
+                        Console.Write("\nEnter Player Name: ");
+                        plr.PlayerName=Console.ReadLine();
+                        Console.Write("\nEnter Player Age: ");
+                        plr.PlayerAge=int.Parse(Console.ReadLine());
+                        odt.Add(plr);
+                        Console.Write("\nPlayer is added successfully ");
+                        break;
+                    case 2:
+                        Console.Write("\nEnter Player Id to Remove: ");
+                        pid = int.Parse(Console.ReadLine());
+                        odt.Remove(pid);
+                        Console.Write("\nPlayer is removed successfully ");
+                        break;
+                    case 3:
+                        Console.Write("\nEnter Player Id: ");
+                        pid = int.Parse(Console.ReadLine());
+                        plr = odt.GetPlayerById(pid);
+                        if (plr == null)
+                        {
+                            Console.Write("\nNo record found ");
+                            break;
+                        }
+                        Console.WriteLine("\n{0}\t{1}\t{2}", plr.PlayerId, plr.PlayerName, plr.PlayerAge);
+                        break;
+                    case 4:
+                        Console.Write("\nEnter Player Name: ");
+                        string pname = Console.ReadLine();
+                        plr=odt.GetPlayerByName(pname);
+                        if (plr == null)
+                        {
+                            Console.Write("\nNo record found ");
+                            break;
+                        }
+                        Console.WriteLine("\n{0}\t{1}\t{2}",plr.PlayerId, plr.PlayerName, plr.PlayerAge);
+                        break;
+                    case 5:
+                        List<Player> allPlayers = odt.GetAllPlayers();
+                        foreach(Player p in allPlayers)
+                        {
+                            Console.Write("\n{0}\t{1}\t{2}",p.PlayerId, p.PlayerName, p.PlayerAge);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                Console.Write("\nDo you want to continue (yes/no)?: ");
+                choice2 = Console.ReadLine();
+            }
+        }
+    }
+}
